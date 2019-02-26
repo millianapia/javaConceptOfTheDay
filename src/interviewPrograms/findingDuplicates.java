1bfdb34920ec53b7b93dc04bcd427268cb0d6b38
millianapia <millianapia@gmail.com>
2019-02-26 01:00:51 +0100

    A program for finding duplicates
    and listing how many times each letter or char is duplicated

---
+++ src/interviewPrograms/findingDuplicates.java
@@ -0,0 +1,33 @@
+package interviewPrograms;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
+/* Finds duplicates and prints how many times*/
+public class findingDuplicates {
+    public static void main(String[] args) {
+        Scanner inn = new Scanner(System.in);
+        System.out.println("Tell me your sentence");
+        String str = inn.nextLine();
+
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+
+        
+        char[] strArray = str.toCharArray();
+        for (char c : strArray) {
+            if (charCountMap.containsKey(c)) {
+                charCountMap.put(c, charCountMap.get(c) + 1);
+            } else {
+                charCountMap.put(c, 1);
+            }
+        }
+
+        Set<Character> charsInString = charCountMap.keySet();
+        for (char ch : charsInString) {
+        if (charCountMap.get(ch) > 1)
+            System.out.println(ch +" : "+ charCountMap.get(ch));
+        }
+
+    }
+}
